d670bce49cd121793fc343f17372b71eb73babf9
Maur025 <mmoyapalli@gmail.com>
2026-08-08 13:41:11 -0400

    feat:kotlin: add description

--- kotlin-lang/src/main/kotlin/kyu8/DoYouSpeakEnglish.kt
+++ kotlin-lang/src/main/kotlin/kyu8/DoYouSpeakEnglish.kt
@@ -1,5 +1,10 @@
 package com.maur025.java.lang.kyu8
 
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
 const val EXPECTED_FRASE = "english"
 
 fun doYouSpeakEnglish(sentence: String?): Boolean {
